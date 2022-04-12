f5a0ad6ce283e0a7d1131d1524990a94102c6e91
vladimir sh <conditer080@gmail.com>
2022-04-12 18:58:32 +0300

    elements + outlets + pic added

---
+++ Netology_IB_Instruments'/Netology_IB_Instruments'/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments'
+//
+//  Created by z on 4/12/22.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var labelName: UILabel!
+    
+    @IBOutlet weak var labelBirthday: UILabel!
+    @IBOutlet weak var labelTown: UILabel!
+    @IBOutlet weak var labelText: UITextView!
+}
