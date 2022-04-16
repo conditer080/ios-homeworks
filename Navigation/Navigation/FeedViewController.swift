b278d873282e5c441aadf647ecd0ef2483360c36
vladimir sh <conditer080@gmail.com>
2022-04-16 21:51:15 +0300

    button added, PostVC added

--- Navigation/Navigation/FeedViewController.swift
+++ Navigation/Navigation/FeedViewController.swift
@@ -11,10 +11,22 @@ class FeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .init(red: 0.3, green: 0.6, blue: 0.1, alpha: 1)
+        makeButton()
     }
-
-
+    
+    private func makeButton() {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        button.center = view.center
+        button.setTitle("PUSH ME", for: .normal)
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    @objc private func tapAction(){
+        let postVC = PostViewController()
+        present(postVC,animated: true)
+    }
+    
 }
 
