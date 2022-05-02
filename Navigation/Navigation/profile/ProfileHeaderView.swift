44ca693597281dc75959fa16da5f2c741133980e
vladimir sh <conditer080@gmail.com>
2022-05-02 16:03:20 +0300

    profile group with profile*.swift added

---
+++ Navigation/Navigation/profile/ProfileHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by z on 5/2/22.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+    
+
+}
---
+++ Navigation/Navigation/profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by z on 5/2/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .lightGray
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
